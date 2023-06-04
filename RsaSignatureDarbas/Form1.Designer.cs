d309d5384fa1f849c9e60b7ff18e62afef587528
GiedriusMauza <giedrius.mauza@gmail.com>
2023-06-04 14:04:48 +0300

    Updated form. Added functionality to send message

--- RsaSignatureDarbas/Form1.Designer.cs
+++ RsaSignatureDarbas/Form1.Designer.cs
@@ -31,13 +31,18 @@
             inputTextBox = new RichTextBox();
             outputTextBox = new RichTextBox();
             btnSign = new Button();
+            messagelb = new Label();
+            signaturelb = new Label();
+            publicKeyTextBox = new RichTextBox();
+            publiclb = new Label();
+            buttonSend = new Button();
             SuspendLayout();
             // 
             // inputTextBox
             // 
-            inputTextBox.Location = new Point(12, 12);
+            inputTextBox.Location = new Point(12, 31);
             inputTextBox.Name = "inputTextBox";
-            inputTextBox.Size = new Size(392, 156);
+            inputTextBox.Size = new Size(242, 79);
             inputTextBox.TabIndex = 0;
             inputTextBox.Text = "";
             // 
@@ -51,7 +56,7 @@
             // 
             // btnSign
             // 
-            btnSign.Location = new Point(347, 208);
+            btnSign.Location = new Point(316, 59);
             btnSign.Name = "btnSign";
             btnSign.Size = new Size(75, 23);
             btnSign.TabIndex = 2;
@@ -59,17 +64,68 @@
             btnSign.UseVisualStyleBackColor = true;
             btnSign.Click += btnSign_Click;
             // 
+            // messagelb
+            // 
+            messagelb.AutoSize = true;
+            messagelb.Location = new Point(16, 7);
+            messagelb.Name = "messagelb";
+            messagelb.Size = new Size(53, 15);
+            messagelb.TabIndex = 3;
+            messagelb.Text = "Message";
+            // 
+            // signaturelb
+            // 
+            signaturelb.AutoSize = true;
+            signaturelb.Location = new Point(453, 249);
+            signaturelb.Name = "signaturelb";
+            signaturelb.Size = new Size(57, 15);
+            signaturelb.TabIndex = 4;
+            signaturelb.Text = "Signature";
+            // 
+            // publicKeyTextBox
+            // 
+            publicKeyTextBox.Location = new Point(16, 277);
+            publicKeyTextBox.Name = "publicKeyTextBox";
+            publicKeyTextBox.Size = new Size(375, 161);
+            publicKeyTextBox.TabIndex = 5;
+            publicKeyTextBox.Text = "";
+            // 
+            // publiclb
+            // 
+            publiclb.AutoSize = true;
+            publiclb.Location = new Point(16, 249);
+            publiclb.Name = "publiclb";
+            publiclb.Size = new Size(62, 15);
+            publiclb.TabIndex = 6;
+            publiclb.Text = "Public Key";
+            // 
+            // buttonSend
+            // 
+            buttonSend.Location = new Point(453, 59);
+            buttonSend.Name = "buttonSend";
+            buttonSend.Size = new Size(75, 23);
+            buttonSend.TabIndex = 7;
+            buttonSend.Text = "Send";
+            buttonSend.UseVisualStyleBackColor = true;
+            buttonSend.Click += buttonSend_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(buttonSend);
+            Controls.Add(publiclb);
+            Controls.Add(publicKeyTextBox);
+            Controls.Add(signaturelb);
+            Controls.Add(messagelb);
             Controls.Add(btnSign);
             Controls.Add(outputTextBox);
             Controls.Add(inputTextBox);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -77,5 +133,10 @@
         private RichTextBox inputTextBox;
         private RichTextBox outputTextBox;
         private Button btnSign;
+        private Label messagelb;
+        private Label signaturelb;
+        private RichTextBox publicKeyTextBox;
+        private Label publiclb;
+        private Button buttonSend;
     }
 }
--- RsaSignatureDarbas/Form1.cs
+++ RsaSignatureDarbas/Form1.cs
@@ -1,3 +1,5 @@
+﻿using System.Net.Sockets;
+using System.Net;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -14,25 +16,84 @@ namespace RsaSignatureDarbas
         {
             using SHA256 alg = SHA256.Create();
 
-            byte[] data = Encoding.ASCII.GetBytes(inputTextBox.ToString());
+            var message = inputTextBox.Text;
+
+            byte[] data = Encoding.ASCII.GetBytes(message);
             byte[] hash = alg.ComputeHash(data);
 
             RSAParameters sharedParameters;
             byte[] signedHash;
 
-            // Generate signature
-            using (RSA rsa = RSA.Create())
+            if (message != String.Empty)
             {
-                sharedParameters = rsa.ExportParameters(false);
+                // Generate signature
+                using (RSA rsa = RSA.Create())
+                {
+                    // Public key
+                    publicKeyTextBox.Text = Convert.ToBase64String(rsa.ExportRSAPublicKey());
 
-                RSAPKCS1SignatureFormatter rsaFormatter = new(rsa);
-                rsaFormatter.SetHashAlgorithm(nameof(SHA256));
+                    sharedParameters = rsa.ExportParameters(false);
 
-                signedHash = rsaFormatter.CreateSignature(hash);
-                string signedHashString = Convert.ToBase64String(signedHash);
-                outputTextBox.Text = signedHashString;
+                    RSAPKCS1SignatureFormatter rsaFormatter = new(rsa);
+                    rsaFormatter.SetHashAlgorithm(nameof(SHA256));
+
+                    signedHash = rsaFormatter.CreateSignature(hash);
+                    string signedHashString = Convert.ToBase64String(signedHash);
+
+                    // Signature
+                    outputTextBox.Text = signedHashString;
+                }
+            }
+            else
+            {
+                publicKeyTextBox.Text = "Enter message first";
+                outputTextBox.Text = "Enter message first";
+            }
+        }
+
+        private async void buttonSend_Click(object sender, EventArgs e)
+        {
+            using Socket client = await SendMessage("M@" + inputTextBox.Text);
+            using Socket client3 = await SendMessage("K@" + publicKeyTextBox.Text);
+            using Socket client4 = await SendMessage("S@" + outputTextBox.Text);
+        }
+
+        private async Task<Socket> SendMessage(String messageText)
+        {
+            IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint ipEndPoint = new(ipAddress, 11_000);
+            Socket client = new(
+                                    ipEndPoint.AddressFamily,
+                                    SocketType.Stream,
+                                    ProtocolType.Tcp);
+
+            await client.ConnectAsync(ipEndPoint);
+            while (true)
+            {
+                // Send message.
+                var message = messageText + "<|EOM|>";
+                var messageBytes = Encoding.UTF8.GetBytes(message);
+                _ = await client.SendAsync(messageBytes, SocketFlags.None);
+                Console.WriteLine($"Socket client sent message: \"{message}\"");
+
+                // Receive ack.
+                var buffer = new byte[1_024];
+                var received = await client.ReceiveAsync(buffer, SocketFlags.None);
+                var response = Encoding.UTF8.GetString(buffer, 0, received);
+                if (response == "<|ACK|>")
+                {
+                    Console.WriteLine(
+                        $"Socket client received acknowledgment: \"{response}\"");
+                    break;
+                }
+                // Sample output:
+                //     Socket client sent message: "Hi friends 👋!<|EOM|>"
+                //     Socket client received acknowledgment: "<|ACK|>"
             }
 
+            client.Shutdown(SocketShutdown.Both);
+            return client;
         }
     }
 }
